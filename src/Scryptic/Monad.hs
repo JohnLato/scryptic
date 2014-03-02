5b086f0105115491ff48213ab5375bff409f34ec
John Lato <jwlato@tsurucapital.com>
2014-03-02 14:56:30 +0900

    add namespace to monad

--- src/Scryptic/Monad.hs
+++ src/Scryptic/Monad.hs
@@ -1,11 +1,17 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Scryptic.Monad (
+  -- * Monadic interface for hooking up the ScryptEngine
   ScrypticM(..),
-  getScryptHooks,
+
+  -- * Creating hooks
   scryptInput,
   scryptOutput,
+  setNamespace,
+  -- ** Using hooks
   joinToEngine,
+  getScryptHooks,
 ) where
 
 import Scryptic.Types
@@ -13,35 +19,56 @@ import qualified Scryptic.Pure as Pure
 import Scryptic.Runtime
 
 import Control.Applicative
+import Control.Lens
 import Control.Monad.State
 
 import Data.Monoid
 import Data.Typeable
 
-newtype ScrypticM a = ScrypticM { runSM :: StateT ScryptHooks IO a }
+data MState = MState
+    { _mScryptHooks :: ScryptHooks
+    , _mNamespace   :: String
+    }
+
+$(makeLenses ''MState)
+
+instance Monoid MState where
+    mempty = MState mempty mempty
+    l `mappend` r = MState (l^.mScryptHooks <> r^.mScryptHooks)
+                           (r^.mNamespace)
+        -- namespace is notionally the same as 'Last String'
+
+newtype ScrypticM a = ScrypticM { runSM :: StateT MState IO a }
     deriving (Functor, Applicative, Monad, MonadIO)
 
-instance MonadState ScryptHooks ScrypticM where
+instance MonadState MState ScrypticM where
     get = ScrypticM get
     put = ScrypticM . put
     state = ScrypticM . state
 
 getScryptHooks :: ScrypticM a -> IO (a,ScryptHooks)
-getScryptHooks m = runStateT (runSM m) mempty
+getScryptHooks m = (fmap . fmap) (view mScryptHooks)
+    $ runStateT (runSM m) mempty
 
--- convenience function to build a 'ScryptHooks' and join it
+-- | convenience function to build a 'ScryptHooks' and join it
 -- to an existing engine.
 joinToEngine :: ScryptEngine -> ScrypticM a -> IO a
 joinToEngine engine m = do
-    (a,scryptic) <- getScryptHooks m
-    joinScryptEngine scryptic engine
+    (a,scryptHooks) <- getScryptHooks m
+    joinScryptEngine scryptHooks engine
     return a
 
 scryptInput :: (Typeable a, Read a) => String -> (a -> IO ()) -> ScrypticM ()
-scryptInput key akt = modify (<> Pure.scryptInput key akt)
+scryptInput key akt = do
+    nm <- use mNamespace
+    mScryptHooks <>= Pure.scryptInput (nm ++ key) akt
 
 scryptOutput :: (Typeable a, Read a, Show a) => String -> ScrypticM (a->IO())
 scryptOutput key = do
-    (akt,scryptic) <- liftIO $ Pure.scryptOutput key
-    modify (<> scryptic)
+    nm <- use mNamespace
+    (akt,scryptHooks) <- liftIO $ Pure.scryptOutput (nm ++ key)
+    mScryptHooks <>= scryptHooks
     return akt
+
+setNamespace :: String -> ScrypticM ()
+setNamespace nm = mNamespace .= nm
