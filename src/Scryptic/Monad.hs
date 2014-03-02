cd667120f1672e2d31c2e02a9c414775ac245b14
John Lato <jwlato@tsurucapital.com>
2014-03-02 15:49:59 +0900

    add subNamespace

--- src/Scryptic/Monad.hs
+++ src/Scryptic/Monad.hs
@@ -9,6 +9,7 @@ module Scryptic.Monad (
   scryptInput,
   scryptOutput,
   setNamespace,
+  subNamespace,
   -- ** Using hooks
   joinToEngine,
   getScryptHooks,
@@ -22,6 +23,7 @@ import Control.Applicative
 import Control.Lens
 import Control.Monad.State
 
+import qualified Data.Map as Map
 import Data.Monoid
 import Data.Typeable
 
@@ -60,15 +62,36 @@ joinToEngine engine m = do
 
 scryptInput :: (Typeable a, Read a) => String -> (a -> IO ()) -> ScrypticM ()
 scryptInput key akt = do
-    nm <- use mNamespace
-    mScryptHooks <>= Pure.scryptInput (nm ++ key) akt
+    key' <- applyNamespace key
+    mScryptHooks <>= Pure.scryptInput (key') akt
 
 scryptOutput :: (Typeable a, Read a, Show a) => String -> ScrypticM (a->IO())
 scryptOutput key = do
-    nm <- use mNamespace
-    (akt,scryptHooks) <- liftIO $ Pure.scryptOutput (nm ++ key)
+    key' <- applyNamespace key
+    (akt,scryptHooks) <- liftIO $ Pure.scryptOutput key'
     mScryptHooks <>= scryptHooks
     return akt
 
+-- | Set a namespace context for following bindings.  Namespaces are
+-- accessed in scrypts as 'namespace.key'
 setNamespace :: String -> ScrypticM ()
 setNamespace nm = mNamespace .= nm
+
+-- | Run a 'ScrypticM' under the current namespace.
+subNamespace :: ScrypticM a -> ScrypticM a
+subNamespace m = do
+    s0 <- get
+    let nm0 = s0^.mNamespace
+    put mempty
+    r <- m
+    newHooks <- use mScryptHooks
+    let newHooks'  = newHooks & over inpMap (Map.mapKeysMonotonic modKey)
+                              & over outMap (Map.mapKeysMonotonic modKey)
+        modKey key = concat [nm0,".",key]
+    put $ s0 & mScryptHooks <>~ newHooks'
+    return r
+
+applyNamespace :: String -> ScrypticM String
+applyNamespace key = do
+    nm <- use mNamespace
+    return $ concat [nm, ".", key]
